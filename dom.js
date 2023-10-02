ea6060d48601d666b33e9588a1d0a940007c0781
yashVSawant <yashsawant23910@gmail.com>
2023-10-02 13:56:06 +0530

    demo file added

--- dom.js
+++ dom.js
@@ -1,5 +1,6 @@
   const header=document.getElementsByClassName('title');
   header[0].style.fontWeight ='bold';
   header[0].style.color ='green';
-  
+  const elem=document.getElementById('main');
+  console.log(elem);
   
